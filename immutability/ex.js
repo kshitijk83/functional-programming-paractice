9fc3093f1e5ef2feb6d4749781d3ff0cd1b3c9ed
kshitijk83 <kshitijk83@gmail.com>
2020-04-21 09:49:05 +0530

    immutability

--- immutability/ex.js
+++ immutability/ex.js
@@ -4,12 +4,18 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber() {}
+function pickNumber(num, nums) {
+	var newArr = [...nums ]
+	if(!newArr.includes(num)){
+		return [num, ...newArr].sort((a,b)=>a-b)
+	}
+	return nums
+}
 
 var luckyLotteryNumbers = [];
 
 while (luckyLotteryNumbers.length < 6) {
-	pickNumber();
+	luckyLotteryNumbers=pickNumber(lotteryNum(), Object.freeze(luckyLotteryNumbers));
 }
 
 console.log(luckyLotteryNumbers);
